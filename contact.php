b132dbb5815ba25e6c05d9291b73adc9f6f4d728
Aurélien Grimaud <aurelien.grimaud@ineedvitamins.fr>
2015-04-07 01:33:13 +0200

    MAJ page d'accueil
    Page d'accueil // Ajout d'une page 404 // Ajout d'une page de
    construction // MAJ style général // .htaccess

--- contact.php
+++ contact.php
@@ -1,7 +1,22 @@
 <?php
+
+	/*
+	Script de contact
+
+	Directeur de la publication: Ian Smallwood
+
+	Contributeurs: Alexis Doyen, Elodie Dassance, Dan Gu, Aurélien Grimaud,
+	Justine Pinaud, Anne Astima, Yann Beduchaud,
+	Yingyin Zhang, Jiazhi Gao, Thibaud Sabathier
+
+	Outils: Webmaster - Un formulaire de contact pour votre site // http://www.commentcamarche.net/faq/4516-webmaster-un-formulaire-de-contact-pour-votre-site // Jean-François PILLOU 
+
+	Développement front-end: Aurélien Grimaud
+	*/
+
 	define( 'MAIL_TO', 'aurelien.grimaud@ineedvitamins.fr' );
 	define( 'MAIL_NAME', '' );
-	define( 'MAIL_FROM', 'mail@domaine.com' );
+	define( 'MAIL_FROM', 'mail@domaine.com"' );
 	define( 'MAIL_OBJECT', '' );
 	define( 'MAIL_MESSAGE', '' );
 
@@ -19,7 +34,7 @@
 		} 
 
 		$from = filter_input( INPUT_POST, 'from', FILTER_VALIDATE_EMAIL );
-		if( $from === NULL OR $from === MAIL_FROM )
+		if( $from === NULL OR $from === MAIL_FROM OR $from === MAIL_TO )
 		{
 			$errors[] = 'Il faudrait votre adresse mail.';
 		}
@@ -44,7 +59,7 @@
 
 		if( count( $errors ) === 0 ) // si il n'y a pas d'erreurs
 		{
-			if( mail( MAIL_TO, $object, $message, "De: $from\nRépondre à: $from\n" ) ) // tentative d'envoi du message
+			if( mail( MAIL_TO, utf8_decode( $object ), $message, "De: $from\nRépondre à: $from\n" ) ) // tentative d'envoi du message
 			{
 				$mailSent = true;
 			}
@@ -60,6 +75,8 @@
 		$from = MAIL_FROM;
 		$object = MAIL_OBJECT;
 		$message = MAIL_MESSAGE;
+		$headers = "De " . $name; 
+		$headers .= 'Content-type: text/html; charset=utf-8'; 
 	}
 
 	if( $mailSent === true ) // si le message a bien été envoyé, on affiche le récapitulatif
@@ -81,7 +98,6 @@
 		<!--STYLE GENERAL-->
 		<link rel="stylesheet" media="all" type="text/css" href="css/foundation.css" />
 		<link rel="stylesheet" media="all" type="text/css" href="css/style.css" />
-		<link rel="stylesheet" media="all" type="text/css" href="css/tomorrow-night.css" />
 
 
 		<!--FONTE-->
@@ -89,28 +105,6 @@
 
 		<!--SCRIPT-->
 		<script type="text/javascript" src="js/vendor/modernizr.js"></script>
-
-	</head>
-
-	<body>
-
-		<section class="blBG adaptableCover flex">
-
-			<article class="row centerBlock card whBG"> 
-				<h1 class="sectionTitle">Votre message a bien été envoyé.</h1>
-				<div class="yourMail">
-					<p><strong class="upCase">nom :</strong><br /><?php echo( $name ); ?></p> 
-					<p><strong class="upCase">adresse email:</strong><br /><?php echo( $from ); ?></p>
-					<p><strong class="upCase">thème :</strong><br /><?php echo( $object ); ?></p>
-					<p><strong class="upCase">message :</strong><br /><?php echo( nl2br( htmlspecialchars( $message ) ) ); ?></p>
-				</div>
-				<div class="large-3 medium-3 small-3">
-					<a href="javascript:history.go(-1)" class="button medium centerOrigin animation return blBut"><span class="animation">retour</span></a>
-				</div>
-			</article>
-
-		</section>
-
 		<script src="js/vendor/jquery.js" type="text/javascript"></script>
 		<script src="js/foundation.min.js" type="text/javascript"></script>
 		<script type="text/javascript">
@@ -131,6 +125,27 @@
 			$(document).foundation().ready(imready);
 		</script>
 
+	</head>
+
+	<body>
+
+		<section class="blBG adaptableCover tryAgain flex">
+
+			<article class="row centerBlock card whBG"> 
+				<h1 class="sectionTitle">Votre message a bien été envoyé.</h1>
+				<div class="yourMail">
+					<p><strong class="upCase">nom :</strong><br /><?php echo( utf8_decode( $name ) ); ?></p> 
+					<p><strong class="upCase">adresse email:</strong><br /><?php echo( $from ); ?></p>
+					<p><strong class="upCase">thème :</strong><br /><?php echo( utf8_decode( $object ) ); ?></p>
+					<p><strong class="upCase">message :</strong><br /><?php echo( nl2br( htmlspecialchars( $message ) ) ); ?></p>
+				</div>
+				<div class="medium-3 columns">
+					<a href="javascript:history.go(-1)" class="button medium centerOrigin animation return blBut"><span class="animation">retour</span></a>
+				</div>
+			</article>
+
+		</section>
+
 	</body>
 
 </html>
@@ -148,14 +163,14 @@
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<meta http-equiv="Content-Style-Type" content="text/css" />
 			<meta name="author" content="Aurélien Grimaud" />
-			<meta name="description" content="Page de contact, dans un contexte de réussite" />
+			<meta name="description" content="Page de contact, dans un contexte d'échec'" />
 
 			<title>Ouuups, petite erreur</title>
 
 			<!--STYLE GENERAL-->
 			<link rel="stylesheet" media="all" type="text/css" href="css/foundation.css" />
 			<link rel="stylesheet" media="all" type="text/css" href="css/style.css" />
-			<link rel="stylesheet" media="all" type="text/css" href="css/tomorrow-night.css" />
+			
 
 
 			<!--FONTE-->
@@ -163,68 +178,6 @@
 
 			<!--SCRIPT-->
 			<script type="text/javascript" src="js/vendor/modernizr.js"></script>
-
-		</head>
-
-		<body>
-			
-			<section class="blBG adaptableCover flex">
-
-				<article class="row centerBlock card whBG">
-
-					<h1 class="sectionTitle">Ouuups, quelques petites erreurs...</h1>
-
-					<?php
-						if( count( $errors ) !== 0 )
-						{
-							echo( "\t\t<ul class=\"txtList errorList\">\n" );
-							foreach( $errors as $error )
-							{
-								echo( "\t\t\t<li class=\"redColor\">$error</li>\n" );
-							}
-							echo( "\t\t</ul>\n" );
-						}
-						else
-						{
-							echo( "\t\t<p id=\"welcome\"><em>Tous les champs sont obligatoires</em></p>\n" );
-						}
-					?> 
-
-					<form id="recupForm" class="row" method="post" action="contact.php">
-						<div class="large-6 medium-6 small-6 columns">
-
-							<label for="name" class="blkColor upCase level3">
-								nom *
-								<input id="name" type="text" name="name" value="<?php echo( $name ); ?>" />
-							</label>
-							<label for="from" class="blkColor upCase level3">
-								adresse e-mail *
-								<input id="from" type="text" name="from" value="<?php echo( $from ); ?>" placeholder="mail@domaine.com"/>
-							</label>
-							<label for="object" class="blkColor upCase level3">
-								thème *
-								<input id="object" type="text" name="object" value="<?php echo( $object ); ?>" />
-							</label>
-
-						</div>
-
-						<div class="large-6 medium-6 small-6 columns">
-
-							<label for="message" class="blkColor upCase level3">
-								message *
-								<textarea id="message" type="text" name="message"><?php echo( $message ); ?></textarea>
-							</label>
-							<div class="sendBut">
-								<button type="submit" name="send" class="button medium centerOrigin animation send blBut"><span class="animation">envoyer</span></button>
-							</div>
-
-						</div>
-					</form>
-
-				</article>
-
-			</section>
-
 			<script src="js/vendor/jquery.js" type="text/javascript"></script>
 			<script src="js/foundation.min.js" type="text/javascript"></script>
 			<script type="text/javascript">
@@ -247,13 +200,18 @@
 				error = function() {
 					<?php 
 					$alert = '#FF2744';
-					if( $name === NULL OR $object === false OR empty( $name ) OR $name === MAIL_OBJECT )
+					if( $name === NULL OR $name === false OR empty( $name ) OR $name === MAIL_NAME )
 					{
 					?>
 						$('input#name').css('border', '2px solid <?php echo $alert; ?>');
 					<?php }; 
 
-					if( $from === NULL OR $from === false OR empty( $from ) OR $from === MAIL_TO OR $from === MAIL_FROM )
+					if( $from === NULL OR empty( $from ) OR $from === MAIL_TO OR $from === MAIL_FROM )
+					{
+					?>
+						$('input#from').css('border', '2px solid <?php echo $alert; ?>');
+					<?php }
+					elseif( $from === false )
 					{
 					?>
 						$('input#from').css('border', '2px solid <?php echo $alert; ?>');
@@ -276,6 +234,68 @@
 
 				$(document).foundation().ready(imready);
 			</script>
+
+
+		</head>
+
+		<body>
+			
+			<section class="blBG adaptableCover tryAgain flex">
+
+				<article class="row centerBlock card whBG">
+
+					<h1 class="sectionTitle">Ouuups, quelques petites erreurs...</h1>
+
+					<?php
+						if( count( $errors ) !== 0 )
+						{
+							echo( "\t\t<ul class=\"txtList errorList\">\n" );
+							foreach( $errors as $error )
+							{
+								echo( "\t\t\t<li class=\"redColor\">$error</li>\n" );
+							}
+							echo( "\t\t</ul>\n" );
+						}
+						else
+						{
+							echo( "\t\t<p id=\"welcome\"><em>Tous les champs sont obligatoires</em></p>\n" );
+						}
+					?> 
+
+					<form id="recupForm" class="row" method="post" action="contact.php" autocomplete="on">
+						<div class="medium-6 columns">
+
+							<label for="name" class="blkColor upCase level3">
+								nom *
+								<input id="name" type="text" name="name" value="<?php echo( utf8_decode( $name ) ); ?>" />
+							</label>
+							<label for="from" class="blkColor upCase level3">
+								adresse e-mail *
+								<input id="from" type="text" name="from" value="<?php echo( $from ); ?>" placeholder="mail@domaine.com"/>
+							</label>
+							<label for="object" class="blkColor upCase level3">
+								thème *
+								<input id="object" type="text" name="object" value="<?php echo( utf8_decode( $object ) ); ?>" />
+							</label>
+
+						</div>
+
+						<div class="medium-6 columns">
+
+							<label for="message" class="blkColor upCase level3">
+								message *
+								<textarea id="message" type="text" name="message"><?php echo( $message ); ?></textarea>
+							</label>
+							<div class="sendBut">
+								<button type="submit" name="send" class="button medium centerOrigin animation send blBut"><span class="animation">envoyer</span></button>
+							</div>
+
+						</div>
+					</form>
+
+				</article>
+
+			</section>
 
 		</body>
 
---
+++ js/plugins/jquery.asset.js
@@ -0,0 +1,127 @@
+/*
+Javascript pour les templates de page de tutoriel
+
+Directeur de la publication: Ian Smallwood
+
+Contributeurs: Alexis Doyen, Elodie Dassance, Dan Gu, Aurélien Grimaud,
+Justine Pinaud, Anne Astima, Yann Beduchaud,
+Yingyin Zhang, Jiazhi Gao, Thibaud Sabathier
+
+Librairies: JQuery, JQuery UI, Highlight.js
+
+Développement front-end: Aurélien Grimaud
+*/
+
+// ensemble des scripts
+imready = function() {
+	cover();
+	itemsRatio();
+	centerTitle();
+	smoothScroll();
+	openRwdMenu();
+	initSnippet();
+}
+
+// couverture adaptable
+cover = function() {
+
+	var innerHeight = (window.innerHeight);
+	$('.adaptableCover').css('height', +innerHeight+"px");
+	$(window).on('resize', function() {
+		$('.adaptableCover').css('height', +innerHeight+"px");
+	})
+
+}
+
+// avoir un carré peu importe la taille
+itemsRatio = function() {
+	var widthRef = $(".topicsWrapper li .urDesign").width();
+
+	$('.flexMosaic li .urDesign').css('height', widthRef +'px');
+
+	$(window).on('resize', function() {
+		var widthRef = $(".flexMosaic li .urDesign").width();
+		$('.flexMosaic li .urDesign').css('height', widthRef +'px');
+	})
+}
+
+// centrer le titre
+centerTitle = function() {
+	var heightRef = ($(".flexMosaic li .urDesign").height())/4;
+
+	$('.topicsWrapper li span h2').css({
+									'transform': 'translateY('+heightRef +'px)',
+									'-webkit-transform': 'translateY('+heightRef +'px)',
+									'-moz-transform': 'translateY('+heightRef +'px)'
+								});
+
+	$('.topicsWrapper li span').on({
+		mouseenter: function(event){
+			var parent = $(event.target).parent();
+			var title = parent.find("h2");
+			title.css({
+						'transform': 'translateY(0)',
+						'-webkit-transform': 'translateY(0)',
+						'-moz-transform': 'translateY(0)'
+					})
+		},
+		mouseleave: function(event){
+			var parent = $(event.target).parent();
+			var title = parent.find("h2");
+			title.css({
+						'transform': 'translateY('+heightRef +'px)',
+						'-webkit-transform': 'translateY('+heightRef +'px)',
+						'-moz-transform': 'translateY('+heightRef +'px)'
+					})
+		}
+	});
+}
+
+// initialisation du style de snippets
+initSnippet = function() {
+
+	hljs.initHighlightingOnLoad();
+	$('pre.snippet code').each(function(i, block) {
+		hljs.highlightBlock(block);
+	});
+
+	hljs.configure({
+		tabReplace: '    ',
+		classPrefix: 'hljs',
+		languages: 'html, css'
+	})
+
+}
+
+// défilement fluide
+smoothScroll = function() {
+
+	$('ol.contents a, .ttBottom a, nav a.snLink, #mainNav a').click(function() {
+		var page = $(this).attr('href');
+		var speed = 300;
+		var headerHeight = $('header').height();
+		var scroll = ($(page).offset().top) - headerHeight;
+
+		$('html, body').animate({scrollTop: scroll}, speed);
+		return false;
+	});
+
+	$('.ttTop').click(function() {
+		$('html, body').animate({scrollTop: 0}, 300);
+		return false;
+	})
+
+}
+
+// ouvrir le menu en responsive
+openRwdMenu = function() {
+
+	$('#toggleMenu').click( function() {
+		$('body, header').toggleClass('pushBody', 150, "linear");
+		$('#rwdNav').toggleClass('pushMenu', 150, "linear");
+		$('#toggleMenu i.burgerIcn').toggleClass('closeIcn');
+	})
+
+}
+
+$(document).foundation().ready(imready);
